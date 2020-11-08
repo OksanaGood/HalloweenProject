40853447d56e8f16121f62f930c60ba306ccb57f
OksanaGood <oksana.shnurkina.76@mail.ru>
2020-11-08 00:02:55 -0500

    no treat or trick commit

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -1,4 +1,10 @@
 public class HalloweenTest {
-    //I am about to give everyone candy
+    //I am about to give everyone candy for halloween
+    //are we there yet?? Hello Hello
+    //test play a game
+    //its fun to drive so fare
+
+    //there will be no treat and trick this year
+    //stay home and be safe and wear mask outside
 
 }
